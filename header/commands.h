e598c4fc5b874d0feb2e80a4e423686c408f2675
bstoney15 <bstonestreet9@gmail.com>
2024-10-21 12:08:52 -0500

    cat implementation added, not tested robustly

--- header/commands.h
+++ header/commands.h
@@ -61,7 +61,7 @@ void commands(char** tl, char* currentInput, int fd)
         }
 
         waitpid(pid, &status, 0);
-    }
+    }// If fromProcess is not an empty string, pass it as input to grep
     else if(strcmp(tl[0], "ls") == 0)
     {   
         int status;
@@ -88,7 +88,6 @@ void commands(char** tl, char* currentInput, int fd)
                 sprintf(cmdbuf, "%s/%s", q.cDir, tl[1]);
                 execl("/bin/ls", "ls", cmdbuf, NULL);
 
-                exit(0); 
             }
 
         }
@@ -111,7 +110,6 @@ void commands(char** tl, char* currentInput, int fd)
             }
             getcwd(q.cDir, sizeof(q.cDir));
         }
-
         return;
     }
     else if(strcmp(tl[0], "pwd") == 0)
@@ -131,13 +129,142 @@ void commands(char** tl, char* currentInput, int fd)
         }
         setenv(tl[1], tl[2], 0);
     }
+    else if(strcmp(tl[0], "cat") == 0)
+    {
+        int cIndex = 0;    // Index for iterating
+        int isHereDoc = 0; // Flag for `<<`
+        int isFileInput = 0; // Flag for `<`
+        int isOutputRedir = 0; // Flag for `>` or `>>`
+        int isAppend = 0;   // Flag for `>>`
+        char *outputFile = NULL;
+
+        // Check for `<<`, `<`, `>`, or `>>` in the command
+        while(tl[cIndex] != NULL && strcmp(tl[cIndex], "|") != 0)
+        {
+            if(strcmp(tl[cIndex], "<<") == 0)
+            {
+                isHereDoc = 1;
+                break;
+            }
+            else if(strcmp(tl[cIndex], "<") == 0)
+            {
+                isFileInput = 1;
+                break;
+            }
+            else if(strcmp(tl[cIndex], ">") == 0)
+            {
+                isOutputRedir = 1;
+                outputFile = tl[cIndex + 1];  // File comes after `>`
+                break;
+            }
+            else if(strcmp(tl[cIndex], ">>") == 0)
+            {
+                isOutputRedir = 1;
+                isAppend = 1;
+                outputFile = tl[cIndex + 1];  // File comes after `>>`
+                break;
+            }
+            cIndex++;
+        }
+
+        int status;
+        int pid = fork();
+        if(pid == 0)  // Child process
+        {
+            if(isHereDoc)
+            {
+                char buffer[1024];
+                char *delimiter = tl[cIndex + 1];  // Delimiter comes after `<<`
+
+                int inputFD[2];
+                pipe(inputFD);
+
+                while (1)
+                {
+                    printf("> ");
+                    fgets(buffer, sizeof(buffer), stdin);
+
+                    // Remove newline character from the input
+                    buffer[strcspn(buffer, "\n")] = '\0';
+
+                    // Break if the delimiter is reached
+                    if(strcmp(buffer, delimiter) == 0)
+                        break;
+
+                    // Write the buffer to the pipe
+                    write(inputFD[1], buffer, strlen(buffer));
+                    write(inputFD[1], "\n", 1);  // Add newline after each input
+                }
+
+                close(inputFD[1]);  // Close write end after writing
+                dup2(inputFD[0], STDIN_FILENO);
+                close(inputFD[0]);  // Close read end after redirection
+            }
+            else if(isFileInput)
+            {
+                char *filename = tl[cIndex + 1];  // Filename comes after `<`
+
+                // Open the file for reading
+                FILE *file = fopen(filename, "r");
+                if(file == NULL)
+                {
+                    perror("fopen failed");
+                    exit(EXIT_FAILURE);
+                }
+
+                int fileFD = fileno(file);  // Get the file descriptor
+                dup2(fileFD, STDIN_FILENO); // Redirect stdin to the file
+                fclose(file);  // Close the file pointer, file descriptor stays open
+            }
+
+            if(isOutputRedir)
+            {
+                // Open the file for appending if `>>`, otherwise open for writing (`>`)
+                FILE *file = fopen(outputFile, isAppend ? "a" : "w");
+                if(file == NULL)
+                {
+                    perror("fopen failed");
+                    exit(EXIT_FAILURE);
+                }
+
+                int fileFD = fileno(file);  // Get the file descriptor
+                dup2(fileFD, STDOUT_FILENO); // Redirect stdout to the file
+                fclose(file);  // Close the file pointer, file descriptor stays open
+            }
+            else if(fd != -1)
+            {
+                dup2(fd, STDOUT_FILENO);
+                close(fd);  // Close after redirection
+            }
+
+            // Prepare arguments for `cat`
+            char *args[cIndex + 1];
+            args[0] = "cat";
+            for (int i = 1; i < cIndex; i++)
+            {
+                if(strcmp(tl[i], ">") == 0 || strcmp(tl[i], ">>") == 0 || strcmp(tl[i], "<") == 0) break;
+                args[i] = tl[i];
+            }
+            args[cIndex] = NULL;
+
+            // Execute `cat`
+            execvp("cat", args);
+            perror("execvp failed");
+            exit(EXIT_FAILURE);
+        }
+
+        waitpid(pid, &status, 0);
+    }
+
+
+
     else
     {
         printf("QUASH: unknown command: %s\n", tl[0]);
     }
 
     
-    }
+}
 
 
 
